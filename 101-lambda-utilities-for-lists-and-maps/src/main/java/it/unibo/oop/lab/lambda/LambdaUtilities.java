65b51b11112ad77f80094047858c637416558369
Alessandra Versari - alessandra.versari2@studio.unibo.it <alessandra.versari2@studio.unibo.it>
2022-11-29 16:08:35 +0100

    completed exercise 101, exercise 102 to correct

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -1,7 +1,9 @@
 package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -58,10 +60,11 @@ public final class LambdaUtilities {
      *         otherwise.
      */
     public static <T> List<Optional<T>> optFilter(final List<T> list, final Predicate<T> pre) {
-        /*
-         * Suggestion: consider Optional.filter
-         */
-        return null;
+        final List<Optional<T>> newlist = new ArrayList<>();
+        list.forEach(t -> {
+            newlist.add(Optional.ofNullable(t).filter(pre));
+        });
+        return newlist;
     }
 
     /**
@@ -80,7 +83,15 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R,Set<T>> map = new HashMap<>();
+        list.forEach(t -> {
+            final Set<T> valueSet = new HashSet<>(Arrays.asList(t));
+            map.merge( op.apply(t),valueSet, (t1,t2) ->{
+                t1.addAll(t2);
+                return t1;
+            });
+        });
+        return map;
     }
 
     /**
@@ -101,7 +112,11 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K,V> newmap = new HashMap<>();
+        map.forEach( (k,v) -> {
+           newmap.put(k, v.orElse(def.get()));
+        });
+        return newmap;
     }
 
     /**
